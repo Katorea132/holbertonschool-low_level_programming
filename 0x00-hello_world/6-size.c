e02a4fa47aad68bf0b0fbc0c53d71e785c605e55
Katorea132 <katorea132@gmail.com>
2020-02-05 18:46:53 +0000

    Tried another method that ALSO works

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,15 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	char c;
+	int i;
+	long int li;
+	long long ll;
+	float f;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
