f72245556f1e69ab23417c6fc1746e794db3f76d
Katorea132 <katorea132@gmail.com>
2020-02-06 21:32:24 +0000

    Should've done this one first than 101

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print combinations without repeating
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	for (a = 0; a <= 8; a++)
+	{
+		b = a + 1;
+		for (;b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (!( a == 8 && b == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
