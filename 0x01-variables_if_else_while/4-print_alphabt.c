02ab3d92f67605b796ce3af96e493310ff91a175
Katorea132 <katorea132@gmail.com>
2020-02-06 18:20:03 +0000

    Added challenge 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This will print the abc but the e and q
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i = 'e' || i = 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
