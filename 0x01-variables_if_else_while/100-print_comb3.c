bda71cdd7b6822883c25bad430bab226c052be31
Katorea132 <katorea132@gmail.com>
2020-02-06 21:37:34 +0000

    fixed betty-related troubles

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,15 @@ int main(void)
 {
 	int a;
 	int b;
+
 	for (a = 0; a <= 8; a++)
 	{
 		b = a + 1;
-		for (;b <= 9; b++)
+		for (; b <= 9; b++)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			if (!( a == 8 && b == 9))
+			if (!(a == 8 && b == 9))
 			{
 				putchar(',');
 				putchar(' ');
