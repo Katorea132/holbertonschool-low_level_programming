f7718db0bffba080cdd58e5b3f4451a4dcba146c
Katorea132 <katorea132@gmail.com>
2020-02-06 17:52:06 +0000

    Added challenge 2 after a few fixes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * main - This pritns the whole abc in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
