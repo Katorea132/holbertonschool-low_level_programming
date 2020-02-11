81e37eb0a0060b2ccc48de752d5d958fb4fd4022
Katorea132 <katorea132@gmail.com>
2020-02-10 22:50:54 -0500

    Added attempts

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,95 +1,37 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * print_to_98 - prints to 98
- * @n: This hold the number
- * Return: returns the values
+ * print_to_98 - this prints numbers to 98
+ * @n: This holds the number
+ * Return: nothing
  */
-void print_to_98(int n)
+void printo_to_98(int n)
 {
-	int a = n / 100;
-	int b = (n / 100) / 10;
-	int c = (n / 100) % 10;
-	if (n > 98)
+  if (n < 98)
+    {
+      for (; n == 98; n++)
 	{
-		for (; n == 98; n--)
-		{
-			if ( n >= 100)
-			{
-				_putchar(a + '0');
-				_putchar(b + '0');
-				_putchar(c + '0');
-			}
-			else if ( n >= 10 && n < 100)
-			{
-				_putchar(b + '0');
-				_putchar(c + '0');
-			}
-			else 
-			{
-				_putchar(c + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-		}
+	  printf("%d", n);
+	  if (!( n == 98))
+	    {
+	      printf(", ");
+	    }
 	}
-	else if ( n == 98)
+    }
+  else if (; n > 98; n--)
+    {
+      for (; n == 98; n--)
 	{
-		_putchar(n + '0');
+	  printf("%d", n);
+	  if (!(n == 98))
+	    {
+	      printf(", ");
+	    }
 	}
-	else if ( n > 0 && n < 98)
-	{
-		for (; n == 98; n++)
-		{
-			if (n < 10)
-			{
-				_putchar(c + '0');
-			}
-			else
-			{
-				_putchar(b + '0');
-				_putchar(c + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	else
-	{
-		for (; n == 98; n++)
-		{
-			if ( n < 0)
-			{
-				_putchar('-');
-				if ( n < -100)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(c + '0');
-				}
-				else if (n < -10 && n > 100)
-				{
-					_putchar(b + '0');
-					_putchar(c + '0');
-				}
-				else
-				{
-					_putchar(c + '0');
-				}
-			}
-			else
-			{
-				if ( n < 10)
-				{
-					_putchar(c + '0');
-				}
-				else
-				{
-					_putchar(b + '0');
-					_putchar(c + '0');
-				}
-			}
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
+    }
+  else
+    {
+      printf("%d", n);
+    }
+      printf("\n");
 }
