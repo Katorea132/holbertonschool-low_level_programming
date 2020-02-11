e91116d4f0ed23f8685d83b262f52892690c5c13
Katorea132 <58535805+Katorea132@users.noreply.github.com>
2020-02-10 23:34:28 -0500

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ *main - kinda tired of fibonacci
+ *Return: 0
+ */
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	long c = 0;
+	long d = 0;
+  long e = 4000000;
+	while (a < e && b < e)
+	{
+		if ((b % 2) == 0)
+			c += b;
+		d = a + b;
+		a = b;
+		b = d;
+	}
+	printf("%ld\n", c);
+	return (0);
+}
