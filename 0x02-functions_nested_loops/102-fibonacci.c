c861b8e00862eed6f29147b53fe4a5af79b18477
Katorea132 <58535805+Katorea132@users.noreply.github.com>
2020-02-10 23:23:21 -0500

    Added the code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+/**
+ *main - fibonacci is cool
+ *Return: 0
+ */
+int main(void)
+{
+  int a;
+  long b, c, d;
+
+	b = 0;
+	c = 1;
+	for (a = 0; a < 50; a++)
+	{
+		d = b + c;
+		b = c;
+		c = d;
+		if (a != 49)
+			printf("%ld, ", d);
+		else
+			printf("%ld\n", d);
+	}
+	return (0);
+}
