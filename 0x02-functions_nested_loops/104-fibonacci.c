86e6aae02d8d06b43c3fb1798373e61810988a10
Katorea132 <58535805+Katorea132@users.noreply.github.com>
2020-02-11 07:21:46 -0500

    Fixed some names of my variables

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,22 +6,43 @@
  */
 int main(void)
 {
-	long a = 1;
-	long b = 2;
-	long c = 3;
-  int e;
+	long int a, b, c, d, e, f, halfas, halfbs;
+	long int printfhalf, printshalf;
 
-	printf("1, 2, ");
-	for (e = 3; e <= 98; e++)
+	d = 0;
+	a = 0;
+	b = 1;
+	for (c = 0; c < 91; c++)
 	{
-		c = a + b;
-		printf("%ld", c);
-		a = b;
-		b = c;
-		if (i < 98)
-    {
+		d = a + b;
+		printf("%ld", d);
+		if (c != 97)
 			printf(", ");
-    }
+		a = b;
+		b = d;
+	}
+	e = a / 1000000000;
+	f = a % 1000000000;
+	halfas = b / 1000000000;
+	halfbs = b % 1000000000;
+	while (c < 98)
+	{
+		printfhalf = e + halfas;
+		printshalf = f + halfbs;
+		if (printshalf >= 1000000000)
+		{
+			printshalf %= 1000000000;
+			printfhalf++;
+		}
+		printf("%ld%ld", printfhalf, printshalf);
+		if (c == 97)
+			break;
+		printf(", ");
+		e = halfas;
+		f = halfbs;
+		halfas = printfhalf;
+		halfbs = printshalf;
+		c++;
 	}
 	printf("\n");
 	return (0);
