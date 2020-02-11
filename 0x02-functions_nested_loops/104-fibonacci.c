4dba6999788f040660560eb9aead8fd8f6940f56
Katorea132 <58535805+Katorea132@users.noreply.github.com>
2020-02-11 13:14:41 +0000

    Fixed names of variables

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	long int a, b, c, d, e, f, halfas, halfbs;
-	long int printfhalf, printshalf;
+	long int a, b, c, d, e, f, g, h;
+	long int ph, ps;
 
 	d = 0;
 	a = 0;
@@ -23,25 +23,25 @@ int main(void)
 	}
 	e = a / 1000000000;
 	f = a % 1000000000;
-	halfas = b / 1000000000;
-	halfbs = b % 1000000000;
+	g = b / 1000000000;
+	h = b % 1000000000;
 	while (c < 98)
 	{
-		printfhalf = e + halfas;
-		printshalf = f + halfbs;
-		if (printshalf >= 1000000000)
+		ph = e + g;
+		ps = f + h;
+		if (ps >= 1000000000)
 		{
-			printshalf %= 1000000000;
-			printfhalf++;
+			ps %= 1000000000;
+			ph++;
 		}
-		printf("%ld%ld", printfhalf, printshalf);
+		printf("%ld%ld", ph, ps);
 		if (c == 97)
 			break;
 		printf(", ");
-		e = halfas;
-		f = halfbs;
-		halfas = printfhalf;
-		halfbs = printshalf;
+		e = g;
+		f = h;
+		g = ph;
+		h = ps;
 		c++;
 	}
 	printf("\n");
