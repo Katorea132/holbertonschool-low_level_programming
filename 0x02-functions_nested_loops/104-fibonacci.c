dffe89582a726a8b157ad71aa764985cc53438fa
Katorea132 <58535805+Katorea132@users.noreply.github.com>
2020-02-10 23:43:18 -0500

    Added the code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+
+/**
+ * main - Hate fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	long c = 3;
+  int e;
+
+	printf("1, 2, ");
+	for (e = 3; e <= 98; e++)
+	{
+		c = a + b;
+		printf("%ld", c);
+		a = b;
+		b = c;
+		if (i < 98)
+    {
+			printf(", ");
+    }
+	}
+	printf("\n");
+	return (0);
+}
