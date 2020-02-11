5553a18bd383cb74ee5ec6cda3972c4f0e25f520
Katorea132 <58535805+Katorea132@users.noreply.github.com>
2020-02-10 23:15:31 -0500

    Fixed typos

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main - sums 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	d = 0;
+	for (a = 0; a < 1024; a++)
+	{
+		b = a % 3;
+		c = a % 5;
+		if (b == 0 || c == 0)
+		{
+			d = d + a;
+		}
+	}
+	printf("%d\n", d);
+	return (0);
+}
