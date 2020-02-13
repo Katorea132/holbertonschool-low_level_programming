9897cf962b944e83c17b5a7402e6655eeecd5eed
Katorea132 <katorea132@gmail.com>
2020-02-13 21:10:06 +0000

    First try

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+	long int a;
+	long int d = 612852475143;
+
+	for (a = 2; a < d; a++)
+	{
+		if (a % d == 0);
+		{
+			d /= a;
+			printf("%ld, ", d);
+		}
+	}
+	printf("%ld\n", d);
+	return (0);
+}
