ef1479f341601f634b283ff7e5e53a8d52186271
Katorea132 <katorea132@gmail.com>
2020-02-17 21:57:04 +0000

    Added exercise 0 and header file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,8 @@
 /**
  * reset_to_98 - resets to 98
- * @*n: Picks the value of the pointer
+ * @n: Picks the value of the pointer
  * Return: none
  */
-void reset_to_98(int *n)
-{
-	*n = 98;
-}/*
-** reset_to_98 - resets to 98
-* @*n: Picks the value of the pointer
-* Return: none
-*/
 void reset_to_98(int *n)
 {
 	*n = 98;
