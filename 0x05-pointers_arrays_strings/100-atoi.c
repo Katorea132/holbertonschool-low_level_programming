4c4474199b99e86fc09d48842aa739ca16382adc
Katorea132 <katorea132@gmail.com>
2020-02-18 20:48:06 +0000

    Added advanced 100 and updated holberton.h

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+
+/**
+ * _atoi - prints digits of a string
+ * @s: Holds the string
+ * Return: returns the value of the integer
+ */
+int _atoi(char *s)
+{
+	int i, sign, digit, logic;
+
+	logic = 0;
+	digit = 0;
+	sign = 1;
+	for (i = 0; s[i] != 0;)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit *= 10;
+			digit -= (s[i] - '0');
+			logic = 1;
+		}
+		else if (logic == 1)
+		{
+			break;
+		}
+		i++;
+
+	}
+	digit *= -sign;
+	return (digit);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
