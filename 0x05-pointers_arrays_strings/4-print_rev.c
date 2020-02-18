ce6712187b05f5213b2ea8229639f55fdf4d26c5
Katorea132 <katorea132@gmail.com>
2020-02-18 14:32:54 +0000

    Added 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - prints in reverse order
+ * @s: Holds the string
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int a;
+	
+
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	a--;
+	for (; a >= 0; a--)
+	{
+
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
