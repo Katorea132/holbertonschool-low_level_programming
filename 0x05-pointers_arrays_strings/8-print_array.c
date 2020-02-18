926f39ef9543b62694d0a2609a78d0bd521feafb
Katorea132 <katorea132@gmail.com>
2020-02-18 16:10:51 +0000

    Added 8 and updated holberton.h

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * print_array - Prints the given amount of values from an array
+ * @a: Holds the array
+ * @n: Holds the number of values to print
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{	
+		if ( b < (n - 1))
+		{
+		printf("%d, ", a[b]);
+		}
+		else if (b == n -1)
+		{
+			printf("%d", a[b]);
+		}
+	}
+	printf("\n");
+}
