7a94630fd5f78d494430a3845e92910dd321c9d0
Katorea132 <katorea132@gmail.com>
2020-02-18 15:13:11 +0000

    Added 6 and updated holberton.h

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - puts every other  character 
+ * @str: Holds the string
+ * Return: none
+ */
+void puts2(char *str)
+{
+	int a, b;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+	}
+	for (b = 0; b < a; b++)
+	{
+		_putchar(str[b]);
+		b++;
+	}
+}
