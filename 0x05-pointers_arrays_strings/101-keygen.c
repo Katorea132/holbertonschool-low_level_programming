3fa0ef40f5c78d78cdde04e9afb1f14a0d9b4a33
Katorea132 <katorea132@gmail.com>
2020-02-18 21:02:45 -0500

    Added 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - 
+ * main - prints a key of 2772 sum of ASCII value
+ * Return: 0
+ */
+int main(void)
+{
+	int b, c;
+
+	srand(time(NULL));
+	for (b = 2772; b > 122;)
+	{
+		c = (rand() % 125);
+		printf("%c", c);
+		b = b - c;
+	}
+	printf("%c", b);
+	return (0);
+}
