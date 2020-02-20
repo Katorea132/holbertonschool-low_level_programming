e6f9a11716212080359216091b33ab80b483201d
Katorea132 <katorea132@gmail.com>
2020-02-20 21:14:24 +0000

    Added a copy from the past exercise

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints a number
+ * @n: stores the given number
+ * Return: none
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+	if (n < 0)
+	{
+		a *= -1;
+		_putchar('-');
+	}
+	if ((a / 10) > 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
+}
