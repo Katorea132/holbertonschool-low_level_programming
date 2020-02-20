05138c72269486c9b408570fd2656c22a8f6e153
Katorea132 <katorea132@gmail.com>
2020-02-20 18:28:16 +0000

    Fixed betty related errors

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,10 +4,11 @@
 * @n: Number of values of the array
  * Return: none
  */
-void reverse_array (int *a, int n)
+void reverse_array(int *a, int n)
 {
 	int b;
 	int c = 0;
+
 	n = n - 1;
 	for (b = 0; b <= n; b++)
 	{
