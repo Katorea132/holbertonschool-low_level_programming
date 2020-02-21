901470f5655c36414a0bd24b0c4012fdda3e32f5
Katorea132 <katorea132@gmail.com>
2020-02-21 00:15:50 +0000

    Added betty fixed and holberton updated 102

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -8,41 +8,24 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
-	int e = 0;
-	int sum = 0;
-	int digit = 0;
-	int digit1 = 0;
-	int rem = 0;
-	int num = 0;
+	int a, b, c, d, e, sum, digit, digit1, rem, num;
 
-	for (a = 0; n1[a] != 0; a++)
-	{
-	}
-	for (b = 0; n2[b] != 0; b++)
-	{
-	}
-
+	a = b = c = d = e = sum = digit = digit1 = rem = num = 0;
+	while (n1[a] != 0)
+		a++;
+	while (n2[b] != 0)
+		b++;
 	if ((a + 2) > size_r || (b + 2) > size_r)
-	{
 		return (0);
-	}
 	a -= 1;
 	b -= 1;
 	for (; a >= 0 || b >= 0;)
 	{
 		digit = digit1 = 0;
 		if (a >= 0)
-		{
 			digit = n1[a--] - '0';
-		}
 		if (b >= 0)
-		{
 			digit1 = n2[b--] - '0';
-		}
 		sum = digit + digit1 + rem;
 		if (sum > 9)
 		{
@@ -50,15 +33,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum -= 10;
 		}
 		else
-		{
 			rem = 0;
-		}
 		r[c++] = (sum + '0');
 	}
 	if (rem == 1)
-	{
 		r[c++] = (1 + '0');
-	}
 	d = c;
 	c -= 1;
 	for (e = 0; e < c; e++, c--)
@@ -70,4 +49,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[d] = '\0';
 	return (r);
 }
-
