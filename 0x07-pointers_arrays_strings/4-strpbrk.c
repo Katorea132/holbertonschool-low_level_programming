96f3b4776af1bd8bb927ec404cadbe55dac530ad
Katorea132 <katorea132@gmail.com>
2020-02-24 19:56:22 +0000

    Added 4 and updated holberton.h

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+/**
+ * _strpbrk - Searches a string for any set of bytes
+ * @s: Holds the string
+ * @accept: Holds the test
+ * Return: returns a pointer char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != 0; a++)
+	{
+		for (b = 0; accept[b] != 0; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				return (&s[a]);
+			}
+		}
+	}
+	return (0);
+}
+
