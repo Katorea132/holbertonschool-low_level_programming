bbada7c1bf6f723b128312a20a61fa11b3444709
Katorea132 <katorea132@gmail.com>
2020-02-24 21:17:22 +0000

    Added 7 and updated holberton.h

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints a chess board
+ * @a: Gets the X
+ * Return: none
+ */
+void print_chessboard(char (*a)[8])
+{
+	int c, b;
+	
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[c][b]);
+		}
+		_putchar('\n');
+	}
+}
