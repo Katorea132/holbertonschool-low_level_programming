536297813ea4ceaf628830ad34d08e376a5bbc8f
Katorea132 <katorea132@gmail.com>
2020-02-24 17:43:32 +0000

    Added a fix to the return

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int b;
-	char *hold = src;
 
 	for (b = 0; b < n; b++)
 	{
 		*(dest + b) = *(src + b);
 	}
-	return (hold);
+	return (dest);
 }
