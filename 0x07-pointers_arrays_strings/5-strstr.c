ecde5e2bc53f7581bed0c039856e0785c540bb8f
Katorea132 <katorea132@gmail.com>
2020-02-24 20:43:08 +0000

    Attempt at fixing

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,13 +11,10 @@ char *_strstr(char *haystack, char *needle)
 
 	for (a = 0; haystack[a] != 0; a++)
 	{
-		if (haystack[a] == needle[c])
+		if (haystack[a] == needle[c] && needle[c] != 0)
 		{
 			c++;
-		}
-		if (needle[c] == 0)
-		{
-			return (haystack[a]);
+			return (&haystack[a]);
 		}
 	}
 	return (0);
