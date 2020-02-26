6f4f750d6b1d4fe9d1957025dee7f2c22d28f2b4
Katorea132 <katorea132@gmail.com>
2020-02-26 22:03:42 +0000

    Added 7 and updated holberton.h

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,33 @@
+/**
+ * swap_test - tests each value of the string
+ * @b: Holds the lenght of the string
+ * @c: Holds the pointer to the initial value
+ * Return: 1 or 0 depending
+ */
+int swap_test(int b, int c, char *d)
+{
+	if (c >= b)
+	{
+		return (1);
+	}
+	else if (d[b] == d[c])
+	{
+		return (swap_test(b - 1, c + 1, d));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if the string is a palindrome
+ * @s: Holds the string
+ * Return: 1 or 0 depending if it is or not
+ */
+int is_palindrome(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	return (swap_test((a - 1), 0, s));
+}
