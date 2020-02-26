32529bcf92cb39e5ffb36c2c687e46042e840d37
Katorea132 <katorea132@gmail.com>
2020-02-26 16:27:29 +0000

    Added from 0 to 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - Returns the lenght of a string
+ * @s: Holds the string
+ * Return: Lenght of the string
+ */
+ int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
