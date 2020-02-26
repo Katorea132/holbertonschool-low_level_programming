1bf9248adb209afe76951164f181741385fa9744
Katorea132 <katorea132@gmail.com>
2020-02-26 19:31:59 +0000

    Attempt 1 at finding primes without recursiveness

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+
+/**
+ * is_prime_number - Tells if a number is a prime or not
+ * @n: Holds the number
+ * Return: 1 or 0 depending if the number is prime or not
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	else if (n % 2 != 0 && n != 2 && n > 1 && n % 5 != 0)
+	{
+		return (1);
+	}
+	return (0);
+}
