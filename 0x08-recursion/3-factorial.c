bb98aeaa67735b131bd174c55edfead8a6b917ca
Katorea132 <katorea132@gmail.com>
2020-02-26 17:50:02 +0000

    Added 3 and updated holberton.h

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+/**
+ * factorial - makes a factorial operation
+ * @n: Holds the number to be factored
+ * Return: the result
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
