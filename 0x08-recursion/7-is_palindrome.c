0229ad9f2abcd02716933502499955bbc597d338
Katorea132 <katorea132@gmail.com>
2020-02-26 22:26:59 +0000

    Added another function to count

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,8 +1,21 @@
-#include "holberton.h"
+/**
+ * _strlen_recursion - Returns the lenght of a string
+ * @s: Holds the string
+ * Return: Lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
 /**
  * swap_test - tests each value of the string
  * @b: Holds the lenght of the string
  * @c: Holds the pointer to the initial value
+ * @d: Holds the string
  * Return: 1 or 0 depending
  */
 int swap_test(int b, int c, char *d)
@@ -25,5 +38,7 @@ int swap_test(int b, int c, char *d)
  */
 int is_palindrome(char *s)
 {
-	return (_strlen_recursion(s - 1), 0, s));
+	int a = _strlen_recursion(s);
+
+	return (swap_test((a - 1), 0, s));
 }
