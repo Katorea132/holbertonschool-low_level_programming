9ea1896caa4956175b5e986bb2ac40fac07cd43e
Katorea132 <katorea132@gmail.com>
2020-02-27 01:04:57 +0000

    Fixed betty related error

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -35,7 +35,7 @@ int wildcmp(char *s1, char *s2)
 			{
 				return (wildcmp(s1 + 1, s2));
 			}
-			else if(*(s2 + 1) == *(s1 + 1))
+			else if (*(s2 + 1) == *(s1 + 1))
 			{
 				return (wildcmp(s1 + 1, s2 + 1));
 			}
@@ -43,27 +43,3 @@ int wildcmp(char *s1, char *s2)
 	}
 	return (0);
 }
-	/**
-	else if (*s1 == 0 || *s2 == 0)
-	{
-		if (*s1 == 0 && *s2 == '*' && *(s2 + 1) == 0)
-		{
-			return (1);
-		}
-		return (0);
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	else if (*s2 == '*' && *(s2 + 1) != 0)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	else if (*s2 == '*' && *(s2 + 1) == 0)
-	{
-		return (wildcmp(s1 + 1, s2));
-	}
-	return (0);
-}
-**/
