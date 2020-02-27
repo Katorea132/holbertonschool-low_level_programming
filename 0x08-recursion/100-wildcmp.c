fbcbbaeec665e65fd28bcc4179e473f619ebd203
Katorea132 <katorea132@gmail.com>
2020-02-26 22:01:26 -0500

    Optimized another attempt

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -17,28 +17,13 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s2 == '*')
 	{
-		if (*(s2 + 1) == 0)
+		if (wildcmp(s1, s2 +1) || wildcmp(s1 + 1, s2))
 		{
 			return (1);
 		}
-		else if (*(s2 + 1) == '*')
+		if (*(s1 + 1) != 0 && *s2 == 0)
 		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		else
-		{
-			if (*(s2 + 1) == *(s1) && *(s2 + 2) == 0)
-			{
-				return (wildcmp(s1, s2 + 1));
-			}
-			else if (*(s2 + 1) != *(s1 + 1))
-			{
-				return (wildcmp(s1 + 1, s2));
-			}
-			else if (*(s2 + 1) == *(s1 + 1))
-			{
-				return (wildcmp(s1 + 1, s2 + 1));
-			}
+			return (0);
 		}
 	}
 	return (0);
