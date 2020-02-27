f3e462269a69284c47f5c27a20672a570c4b4bc3
Katorea132 <katorea132@gmail.com>
2020-02-27 00:48:23 +0000

    Removed 2 else if that were checking separatedly for equality and diferent than null in s2, making it the same to not check it at all

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -21,11 +21,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (1);
 		}
-		else if (*(s2 + 1) == '*' && *(s1 + 1) != 0)
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		else if (*(s2 + 1) == '*' && *(s1 + 1) == 0)
+		else if (*(s2 + 1) == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
