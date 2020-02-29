4e8cb4036363623738a607b69e335eb623732f52
Katorea132 <katorea132@gmail.com>
2020-02-29 13:36:35 -0500

    Deleted a semicolon causing troubles

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - prints the name of the program
  * @argc: Counts the number of arguments entered
  * @argv: Holds the arguments in a string
- * Return: 0;
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
