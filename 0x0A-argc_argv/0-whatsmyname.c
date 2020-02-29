2ae02ee42d8668fca52b7caaa3ed2fc4db498dd1
Katorea132 <katorea132@gmail.com>
2020-02-29 14:01:14 -0500

    Deleted from git and remade it

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints name of itself
+ * @argc: Counts arguments
+ * @argv: Holds arguments in a string way
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+        printf("%s\n", argv[0]);
+        return (0);
+}
