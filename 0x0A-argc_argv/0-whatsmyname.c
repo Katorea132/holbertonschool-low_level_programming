b1c7364da7d622a2641438f3bc83aba8600de4af
Katorea132 <katorea132@gmail.com>
2020-02-29 13:46:39 -0500

    Created and pushed a holberton.h file in case this was the problem

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: Holds arguments in a string way
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
