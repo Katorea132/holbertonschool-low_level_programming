74506a65fde9ca58d6893393295410f50389b141
Katorea132 <katorea132@gmail.com>
2020-03-03 18:06:10 +0000

    Added 3 and updated holberton.h

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - allocates a 2x2 grid
+ * @width: Holds the width
+ * @height: Holds the height
+ * Return: returns a pointer to an int pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (0);
+	}
+	grid = malloc(sizeof(int) * height);
+	if (grid == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == 0)
+		{
+			for (i -= 1; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (0);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
