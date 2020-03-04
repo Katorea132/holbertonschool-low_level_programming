3bc887ddf2e46bcc683cd17c4781bac9f94eef32
Katorea132 <katorea132@gmail.com>
2020-03-03 19:28:38 -0500

    Attempt

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -6,17 +6,42 @@
  */
 char **strtow(char *str)
 {
-	int i, j, k;
+	int i, j, k, lar, val, l;
 	char **sepa;
 
-	i = j = 0;
 	if (str == 0 || str == "")
-	{
 		return (0);
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == 0))
+			val++;
 	}
-	sepa = malloc(sizeof(char) * (i + j + 1));
+	if (val == 0)
+		return (0);
+	sepa = malloc((val + 1) * sizeof(char *));
 	if (sepa == 0)
-	{
 		return (0);
+	j = 0;
+	for (i = 0; str[i] != 0 && j < val; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for (lar = 0, k = i; str[k] != ' ' && str[k] != 0; k++, lar++)
+			{
+			}
+			sepa[j] = malloc((lar + 1) * sizeof(char));
+			if (sepa[j] == 0)
+			{
+				for (j -= 1; j >= 0; j++)
+					free(sepa[j]);
+				free(sepa);
+				return (0);
+			}
+			for (l = 0; l < lar; m++, i++)
+				sepa[j][l] = str[i];
+			sepa[j++][m] = 0;
+		}
 	}
-
+	sepa[j] = 0;
+	return (sepa);
+}
