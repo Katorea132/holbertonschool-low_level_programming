50b08c7a9193c4e8cef1ea3c35598eb447f8b0b7
Smart Guy Cocoa <58535805+Katorea132@users.noreply.github.com>
2020-03-05 21:13:19 -0500

    Attempt at 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,54 +1,80 @@
-#include "holberton.h"
 #include <stdlib.h>
-/**
- * _atoi - prints digits of a string
- * @s: Holds the string
- * Return: returns the value of the integer
- */
-int _atoi(char *s)
-{
-	int i, sign, digit, logic;
-
-	logic = 0;
-	digit = 0;
-	sign = 1;
-	for (i = 0; s[i] != 0;)
-	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit *= 10;
-			digit -= (s[i] - '0');
-			logic = 1;
-		}
-		else if (logic == 1)
-		{
-			break;
-		}
-		i++;
-
-	}
-	digit *= -sign;
-	return (digit);
-}
+#include <stdio.h>
+#include <string.h>
 /**
  * _isdigit - This will tell if the variable is a 0 or a 9
  * @c: This holds the variable
  * Return: 1 or 0 if number or not
  */
-int _isdigit(int c)
+int _isdigit(char *c)
 {
-	if (c <= 57 && c >= 48)
+    int i;
+    
+	for (i = 0; c[i] != 0; i++)
 	{
-		return (1);
+	    if (c[i] < '0' || c[i] > '9')
+	        return (0);
 	}
-	else
+	return (1);
+}
+/**
+ * _memset - Fills the first n bytes of the memory area
+ * @s: points to the area
+ * @b: Holds the constant byte
+ * @n: Determines the amount of bytes
+ * Return: returns a char
+ */
+void *_memset(void *s, int b, unsigned int n)
+{
+    if (n != 0)
+    {
+        char *i = s;
+        do {
+            *i++ = b;
+        } while (--n);
+    }
+	return (s);
+}
+/**
+ * _strlen - It returns the lenght of a string
+ * @s: Holds the string
+ * Return: none
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0';)
+	{
+	   ++a;
+	}
+	return (a);
+}
+/**
+ * _calloc - a function that allocates memory in an array using malloc
+ * @nmemb: Determines the amount of sized bytes
+ * @size: Determines the sizing of the bytes above
+ * Return: a pointer to anything
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *s;
+	char *t;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
 	{
 		return (0);
 	}
+	s = malloc(nmemb * size);
+	if (s == 0)
+	{
+		return (0);
+	}
+	t = s;
+	for (i = 0; i < nmemb * size; i++)
+		t[i] = 0;
+	return (s);
 }
 /**
  * main - prints the multiplication result of 2 numbers
@@ -56,24 +82,52 @@ int _isdigit(int c)
  * @argv: Holds arguments
  * Return: 0
  */
-int main (int argc, char *argv)
+int main (int argc, char *argv[])
 {
-	char *error = "Error\n";
-	char *all;
-	int *mult;
-	int i, j, k;
-
-	if (argc != 3)
-	{
-		for (i = 0; error[i] != 0; i++)
-			_putchar(error[i]);
-		exit(98);
-	}
-	if (_isdigit(argv[1]) == 0 || _isdigit(argv[2]) == 0)
-	{
-		for (i = 0; error[i] != 0; i++)
-			_putchar(error[i]);
-		exit(98);
-	}
-	
+    int i, j, len, siz1, siz2, car, pro;
+    int *res;
+    
+    if (argc != 3 || !(_isdigit(argv[1])) || !(_isdigit(argv[2])))
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    siz1 = _strlen(argv[1]);
+    siz2 = _strlen(argv[2]);
+    len = siz1 + siz2;
+    res = calloc(len, sizeof(int *));
+    if (res = 0)
+    {
+        printf("Error\n");
+        exit (98);
+    }
+    for (i = siz2 - 1; i >= 0; i--)
+    {
+        car = 0;
+        for (j = siz1 - 1; j >= 0; j--)
+        {
+            pro = (argv[2][i] - '0') * (argv[1][j] - '0');
+            car = (pro / 10);
+            res[(i + j) + 1] += (pro % 10);
+            if (res[(i + j) + 1] > 9)
+            {
+                res[i + j] += res[(i + j) + 1] / 10;
+                res[(i + j) + 1] = res[(i + j) + 1] % 10;
+            }
+            res[i + j] += car;
+        }
+    }
+    if (res[0] == 0)
+    {
+        i = 1;
+    }
+    else
+    {
+        i = 0;
+    }
+    for (; i < len; i++)
+        printf("%d", res[i]);
+    printf("\n");
+    free(res);
+    return (0);
 }
