2c8b5a3da76bae7df273e7e4092635775fe85129
Katorea132 <katorea132@gmail.com>
2020-03-09 20:30:26 +0000

    Fixed tertiary operator

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,6 +7,6 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name ? d->name = "(nil)", d->age, d->owner ? d->owner = "(nil)");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name ? d->name : "(nil)", d->age, d->owner ? d->owner : "(nil)");
 }
 
