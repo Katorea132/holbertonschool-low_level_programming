4789afbf0d54dfc63039d4d990c3256ad3eb2955
Katorea132 <katorea132@gmail.com>
2020-03-09 20:33:02 +0000

    Fixed betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,6 +7,7 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name ? d->name : "(nil)", d->age, d->owner ? d->owner : "(nil)");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name ? d->name : "(nil)", d->age,
+			d->owner ? d->owner : "(nil)");
 }
 
