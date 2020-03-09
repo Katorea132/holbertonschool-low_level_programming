711cd85ea9995c7e39ee472bff49d291d4f0a54f
Katorea132 <katorea132@gmail.com>
2020-03-09 21:29:38 +0000

    Attempt at fixing 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,29 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ * @str: Holds the string to be copied
+ * Return: a pointer to a char
+ */
+char *_strdup(char *str)
+{
+	char *strc;
+	int i;
+
+	if (str == 0)
+		return (0);
+	for (i = 0; str[i] != 0; i++)
+	{
+	}
+	strc = malloc((sizeof(char) * 1) + i);
+	if (strc == 0)
+		return (0);
+	for (i = 0; str[i] != 0; i++)
+		strc[i] = str[i];
+	strc[i] = 0;
+	return (strc);
+}
+/**
  * new_dog - Copies name of best boi and the noonecares
  * @name: Holds the name of the best boya
  * @age: Holds the age of the goodest boi
@@ -13,13 +36,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new == 0)
 		return (0);
-	new->name = name;
+	new->name = _strdup(name);
 	if (new->name == 0)
 	{
 		free(new);
 		return (0);
 	}
-	new->owner = owner;
+	new->owner = _strdup(owner);
 	if (new->owner == 0)
 	{
 		free(new->name);
