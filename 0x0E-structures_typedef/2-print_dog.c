58dbfa41afe7e66d8540acea21fc33b77a77408f
Katorea132 <katorea132@gmail.com>
2020-03-09 20:16:34 +0000

    Added 2 and updated dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints info of the goodest boya
+ * @d: Holds the struct that holds that which is sacred
+ * Return: none
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
+
