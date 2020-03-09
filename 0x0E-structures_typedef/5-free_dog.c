520bc04eaa5ac62c988cdc0e8e6653953a7a9df2
Katorea132 <katorea132@gmail.com>
2020-03-09 21:38:33 +0000

    Added 5 and updated dog.h

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - frees the dog
+ * @d: Holds the dog to free
+ * Return: none
+ */
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
