79021c541a642ff4992f2f21349b957900cf5048
Katorea132 <katorea132@gmail.com>
2020-03-11 18:23:02 +0000

    Optimized the code, fixed a typo

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,16 +9,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1)
-		return (-1);
 
-	if (cmp != 0)
-	{
-		if (array == 0)
-			return (-1);
+	if (cmp != 0 && size > 0 && array != 0)
 		for (i = 0; i < size; i++)
-			if (cpm(array[i]))
+			if (cmp(array[i]))
 				return (i);
-	}
 	return (-1);
 }
