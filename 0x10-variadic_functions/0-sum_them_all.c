f5d50c36b73a83122213311bee14a81cafac6be0
Katorea132 <katorea132@gmail.com>
2020-03-13 14:41:26 +0000

    Added point 0, added variadic header and README file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sums them all
+ * @n: Holds the amount of arguments, in a constant
+ * @...: Holds many more arguments :D
+ * Return: an integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list nums;
+
+	va_start(nums, n);
+
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(nums, int);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+# This is a project about variadic functions
+** This will contain stuffs about variadic functions and probably some other stuff as well**
