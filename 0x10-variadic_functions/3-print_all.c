296099709f407b1211b95ecf1343cac720a103bc
Katorea132 <katorea132@gmail.com>
2020-03-13 16:29:22 +0000

    Added first part of 3 and updated variadic header

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,80 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_ all - prints anything that is sent
+ * @format: Holds a string, each character points to a type of data
+ * @...: Holds all the arguments
+ * Return: none
+ */
+/**
+ * car - Prints a character
+ * @arg: Holds the va_list
+ * Return: none
+ */
+void car (va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+ * inte - Prints integers
+ * @arg: Holds the va_list
+ * Return: none
+ */
+void inte (va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+/**
+ * flo - Prints floats
+ * @arg: Holds the va_list
+ * Return: none
+ */
+void flo (va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+ * str - prints a string
+ * @arg: Holds the va_list
+ * Return: none
+ */
+void str (va_list arg)
+{
+	char *s;
+
+	s = va_arg(arg, char *);
+	if (s == 0)
+		printf("(nil)");
+	else
+
+}
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list waido;
+	typ typin[] = {
+		{"c", car},
+		{"i", inte},
+		{"f", flo},
+		{"s", str},
+		{0, 0}
+	};
+
+	i = j = 0;
+	va_start(waido, format);
+	while (format != 0 && format[i] != 0)
+	{
+		while (typin[j].op[0] != 0)
+		{
+			if (typin[j].op[0] == format[i])
+				typin[j].f(waido);
+			if (format[i + 1] != 0)
+				printf(", ");
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(waido);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,19 @@
 #ifndef VAR
 #define VAR
+/**
+ * struct op - Struct op
+ *
+ * @op: The type
+ * @f: The function associated
+ */
+typedef struct type
+{
+	char *op;
+	void (*f)();
+} typ;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
