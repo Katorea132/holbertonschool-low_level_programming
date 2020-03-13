a5f5bfb6ad048c63c6ddef26c788d7883bec8f0d
Katorea132 <katorea132@gmail.com>
2020-03-13 18:00:50 +0000

    Added changes done to sum and print

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,9 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	va_list nums;
 
+	if (n == 0)
+		return (0);
 	va_start(nums, n);
-
 	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(nums, int);
+	va_end(nums);
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(nums, n);
 		for (i = 0; i < n; i++)
 		{
-			printf("%d%s", va_arg(nums, int), separator);
+			if (i < n - 1)
+				printf("%d%s", va_arg(nums, int), separator);
+			else
+				printf("%d", va_arg(nums, int));
 		}
 		va_end(nums);
 	}
