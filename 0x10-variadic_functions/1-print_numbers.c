4b255c138a439805011b1ad44eb4733025eb45bb
Katorea132 <katorea132@gmail.com>
2020-03-13 14:56:01 +0000

    Added 1 and updated variadic header

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: String to be printed between numbers
+ * @n: Holds the amount of arguments
+ * @...: Many more arguments :D
+ * Return: none
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nums;
+
+	if (separator != 0 && n != 0)
+	{
+		va_start(nums, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d%s", va_arg(nums, int), separator);
+		}
+		va_end(nums);
+	}
+	printf("\n");
+}
