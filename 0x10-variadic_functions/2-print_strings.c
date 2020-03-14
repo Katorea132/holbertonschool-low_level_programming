f2b00236ec8798ba9315757f0128f9f2d9c32caa
Katorea132 <katorea132@gmail.com>
2020-03-14 12:47:34 -0500

    Same fix than for 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,28 +13,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *chpr;
 	va_list strs;
 
-	if (separator != 0)
+	va_start(strs, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(strs, n);
-		for (i = 0; i < n; i++)
-		{
-			chpr = va_arg(strs, char *);
-			if (i < n - 1)
-			{
-				if (chpr != 0)
-					printf("%s%s", chpr, separator);
-				else
-					printf("%s%s", "(nil)", separator);
-			}
-			else
-			{
-				if (chpr != 0)
-					printf("%s", chpr);
-				else
-					printf("%s", "(nil)");
-			}
-		}
-		va_end(strs);
+		chpr = va_arg(strs, char *);
+		if (chpr != 0)
+			printf("%s", chpr);
+		else
+			printf("(nil)");
+		if (i < (n - 1) && separator != 0)
+			printf("%s", separator)
 	}
+	va_end(strs);
 	printf("\n");
 }
