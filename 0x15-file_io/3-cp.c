49007925dbbca2cb3d7812b5932bc8b320030c74
Katorea132 <katorea132@gmail.com>
2020-03-31 17:14:14 -0500

    Attempt at fixing 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,8 @@ int main(int argc, char *argv[])
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 		argv[1]), exit(98);
-	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
+	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR |
+	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	for (ccki = 1; ccki;)
