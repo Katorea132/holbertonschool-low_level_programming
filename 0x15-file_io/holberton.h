c4471f7e27d793b869b77b68a2b6567a69d288cc
Katorea132 <katorea132@gmail.com>
2020-03-31 23:01:47 -0500

    Updated holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,5 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void pwasabimore(Elf64_Ehdr h);
+void pmagic(Elf64_Ehdr elfi);
+void pclass(Elf64_Ehdr elfi);
+void pdata(Elf64_Ehdr elfi);
+void pversion(Elf64_Ehdr elfi);
+void pwasabi(Elf64_Ehdr elfi);
+void pabiversion(Elf64_Ehdr elfi);
+void ptype(Elf64_Ehdr elfi);
+void pentry(Elf64_Ehdr elfi);
 
 #endif /* for HOL */
