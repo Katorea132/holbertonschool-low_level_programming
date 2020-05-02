8562498b6caf370873bd65aa7a9b457677943c06
Katorea132 <katorea132@gmail.com>
2020-05-02 15:00:41 -0500

    Added 4, updated lists.h

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - Frees a linked list
+ * @head: Holds the head
+ * Return: none
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *swappity, *tmp;
+	
+	if (!head)
+		return;
+	for(swappity = head->next; swappity; swappity = tmp)
+	{
+		tmp = swappity->next;
+		free(swappity);
+	}
+	free(head);
+}
