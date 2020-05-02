8048a7a769f0597733bd18b15fd32d1571a0fe9a
Katorea132 <katorea132@gmail.com>
2020-05-02 15:01:55 -0500

    FIxed betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,10 +7,10 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *swappity, *tmp;
-	
+
 	if (!head)
 		return;
-	for(swappity = head->next; swappity; swappity = tmp)
+	for (swappity = head->next; swappity; swappity = tmp)
 	{
 		tmp = swappity->next;
 		free(swappity);
