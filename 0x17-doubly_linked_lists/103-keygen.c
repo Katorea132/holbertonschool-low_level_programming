6472d97232d116f337ece45963caa80f0119f0e1
Katorea132 <katorea132@gmail.com>
2020-05-03 00:50:38 -0500

    F1 and F2 passed

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -17,13 +17,13 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 		printf("Usage: ./keygen <Username>\n"), exit(1);
 	len = strlen(argv[1]);
-	password[0] = string[(len ^ 59) & 63]; //Simply XOR and AND operations over the lenght, with 59 and 63 respectively
+	password[0] = string[(len ^ 59) & 63]; //Simply XOR and AND operations over the lenght, with 59 and 63 respectively - WORKING
 	printf("%c\n", password[0]);
 	for (i = 0; i < len; i++)
 	{
 		ops += argv[1][i]; //This is a loop through the username, addign each value and incrementing I
 	}
-	password[1] = string[(ops ^ 79) & 63]; //Again, XOR and AND with 85 and 63 over the result of the addition
+	password[1] = string[(ops ^ 79) & 63]; //Again, XOR and AND with 85 and 63 over the result of the addition - WORKING
 	printf("%c\n", password[1]);
 	return (0);
 }
