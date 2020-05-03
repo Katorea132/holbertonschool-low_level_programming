0a571864433daf6d0dfd4d037986c56069ab0c32
Katorea132 <katorea132@gmail.com>
2020-05-03 00:40:13 -0500

    Saving part of the code just in case

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - main
+ * @argc: Holds the argument count
+ * @argv: Holds the arguments
+ * Return: 0 on success 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, len, ops = 0;
+	char string[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char password[5];
+
+	if (argc != 2)
+		printf("Usage: ./keygen <Username>\n"), exit(1);
+	len = strlen(argv[1]);
+	password[0] = string[(len ^ 59) & 63]; //Simply XOR and AND operations over the lenght, with 59 and 63 respectively
+	printf("%c\n", password[0]);
+	for (i = 0; i < len; i++)
+	{
+		ops += argv[1][i]; //This is a loop through the username, addign each value and incrementing I
+	}
+	password[1] = string[(ops ^ 79) & 63]; //Again, XOR and AND with 85 and 63 over the result of the addition
+	printf("%c\n", password[1]);
+	return (0);
+}
