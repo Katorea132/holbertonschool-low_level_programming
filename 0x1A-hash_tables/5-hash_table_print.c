0e2c288c71e4314d99a36de9e084c06768d68d49
Katorea132 <katorea132@gmail.com>
2020-05-29 14:20:50 -0500

    FIxed wrong format of printign

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,17 +9,18 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *tmp = 0;
+	char found = 'n';
 
 	if (!ht)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
-		if (tmp)
+		for (tmp = ht->array[i]; tmp; tmp = tmp->next)
 		{
-			printf("'%s': '%s'", tmp->key, tmp->value);
-			for (; tmp; tmp = tmp->next)
+			if (found == 'n')
+				printf("'%s': '%s'", tmp->key, tmp->value), found = 'y';
+			else
 				printf(", '%s': '%s'", tmp->key, tmp->value);
 		}
 	}
