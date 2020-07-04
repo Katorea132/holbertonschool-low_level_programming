ee7eb47e4b00a56d04f724281b758f189d3b1bba
Katorea132 <katorea132@gmail.com>
2020-07-03 20:53:40 -0500

    Added 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    peri, x = 0, 0
+    for row in grid:
+        if len(grid) <= x + 1:
+            break
+        for y in range(0, len(row)):
+            if grid[x][y] == 1:
+                if grid[x][y + 1] == 0:
+                    peri += 1
+                if grid[x][y - 1] == 0:
+                    peri += 1
+                if grid[x + 1][y] == 0:
+                    peri += 1
+                if grid[x - 1][y] == 0:
+                    peri += 1
+        x += 1
+    return peri
