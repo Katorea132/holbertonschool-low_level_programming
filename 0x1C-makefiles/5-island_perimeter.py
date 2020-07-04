1b4cfcaa7a4c9da75ab0fb5b83ad7e8c0f0882a4
Katorea132 <katorea132@gmail.com>
2020-07-03 21:01:29 -0500

    Trying to fix missing requirement

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,7 @@
 """THis module contains the function to find the perimeter of an island
 """
 
+
 def island_perimeter(grid):
     """Function to find the perimeter of a island
 
@@ -11,11 +12,9 @@ def island_perimeter(grid):
     Returns:
         int: Perimeter of such island
     """
-    peri, x = 0, 0
-    for row in grid:
-        if len(grid) <= x + 1:
-            break
-        for y in range(0, len(row)):
+    peri = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 if grid[x][y + 1] == 0:
                     peri += 1
@@ -25,5 +24,4 @@ def island_perimeter(grid):
                     peri += 1
                 if grid[x - 1][y] == 0:
                     peri += 1
-        x += 1
     return peri
